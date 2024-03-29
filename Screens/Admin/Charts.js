c2553e5166fe43deff24e7553b56dabb84bd093a
baluyotkaylamae <kaylamae.baluyot@tup.edu.ph>
2024-03-29 15:22:20 +0800

    product count in stocks barchart

--- Screens/Admin/Charts.js
+++ Screens/Admin/Charts.js
@@ -3,7 +3,6 @@ import { View, Text, StyleSheet, Dimensions } from "react-native";
 import { LineChart, BarChart, PieChart } from "react-native-chart-kit";
 import axios from "axios";
 import baseURL from "../../assets/common/baseurl";
-import TotalSales from "./TotalSales";
 
 const Chart = () => {
   const [orders, setOrders] = useState([]);
@@ -85,43 +84,88 @@ const Chart = () => {
 
   return (
     <View>
-      <Text>Total Sales by Date</Text>
-      <LineChart
-        data={{
-          labels: labels,
-          datasets: [
-            {
-              data: data,
+      <View style={styles.chartContainer}>
+        <Text>Total Sales by Date</Text>
+        <LineChart
+          data={{
+            labels: labels,
+            datasets: [{ data: data }],
+          }}
+          width={Dimensions.get("window").width}
+          height={220}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1}
+          chartConfig={{
+            backgroundColor: "#e26a00",
+            backgroundGradientFrom: "#fb8c00",
+            backgroundGradientTo: "#ffa726",
+            decimalPlaces: 2,
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
             },
-          ],
-        }}
-        width={Dimensions.get("window").width}
-        height={220}
-        yAxisLabel="$"
-        yAxisSuffix="k"
-        yAxisInterval={1}
-        chartConfig={{
-          backgroundColor: "#e26a00",
-          backgroundGradientFrom: "#fb8c00",
-          backgroundGradientTo: "#ffa726",
-          decimalPlaces: 2,
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          style: {
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "#ffa726",
+            },
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
             borderRadius: 16,
-          },
-          propsForDots: {
-            r: "6",
-            strokeWidth: "2",
-            stroke: "#ffa726",
-          },
-        }}
-        bezier
-        style={{
-          marginVertical: 8,
-          borderRadius: 16,
-        }}
-      />
+          }}
+        />
+      </View>
+      <View style={styles.chartContainer}>
+        <Text>Product Counts in Stocks</Text>
+        <BarChart
+          data={{
+            labels: productLabels,
+            datasets: [{ data: productData }],
+          }}
+          width={Dimensions.get("window").width}
+          height={220}
+          yAxisLabel="#"
+          chartConfig={{
+            backgroundColor: "#e26a00",
+            backgroundGradientFrom: "#fb8c00",
+            backgroundGradientTo: "#ffa726",
+            decimalPlaces: 0,
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+          }}
+        />
+      </View>
+      {/* <View style={styles.chartContainer}>
+        <Text>Most Bought Products</Text>
+        <PieChart
+          data={pieChartData}
+          width={Dimensions.get("window").width}
+          height={220}
+          chartConfig={{
+            backgroundColor: "#e26a00",
+            backgroundGradientFrom: "#fb8c00",
+            backgroundGradientTo: "#ffa726",
+            decimalPlaces: 0,
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+          }}
+          accessor="count"
+          backgroundColor="transparent"
+          paddingLeft="15"
+          absolute
+          legendData={pieChartLabels}
+        />
+      </View> */}
     </View>
   );
 };
